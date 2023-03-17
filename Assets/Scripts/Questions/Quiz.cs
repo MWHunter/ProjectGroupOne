6d9cc65f2397985d3f85a6000b79a6e9a22ff36b
Selahaddin Turkoglu <90051755+turkogl3@users.noreply.github.com>
2023-03-16 20:58:08 -0500

    Implemented a passing to a next question functionality
    Added 3 questions and fixed couple of bugs regarding question UI

--- Assets/Scripts/Questions/Quiz.cs
+++ Assets/Scripts/Questions/Quiz.cs
@@ -7,42 +7,90 @@ using UnityEngine.UI;
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
-    [SerializeField] QuestionSO question;
+    [SerializeField] List<QuestionSO> questions = new List<QuestionSO>();
+    QuestionSO currentQuestion;
     [SerializeField] GameObject[] answerButtons;
-    int correctAnswerIndex;
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
+    [SerializeField] Button nextQuestionButton;
 
     void Start()
     {
-        questionText.text = question.getQuestion();
-
-        for(int i = 0; i < answerButtons.Length; i++){
-
-            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.getAnswer(i);
-        }
-       
+        GetNextQuestion();
     }
 
     public void OnAnswerSelected(int index)
     {
         Image buttonImage;
 
-        if(index == question.getCorrectAnswerIndex())
+        if(index == currentQuestion.getCorrectAnswerIndex())
         {
             questionText.text = "Correct!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
-        }else
+        }
+        else
         {
-            correctAnswerIndex = question.getCorrectAnswerIndex();
-            string correctAnswer = question.getAnswer(correctAnswerIndex);
+            int correctAnswerIndex = currentQuestion.getCorrectAnswerIndex();
+            string correctAnswer = currentQuestion.getAnswer(correctAnswerIndex);
             questionText.text = "Sorry, the correct answer was;\n" + correctAnswer;
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
         }
+
+        SetButtonState(false);
+        nextQuestionButton.gameObject.SetActive(true);
     }
 
-   
+    public void OnNextQuestionButtonPressed()
+    {
+        nextQuestionButton.gameObject.SetActive(false);
+        GetNextQuestion();
+    }
+
+    void GetNextQuestion()
+    {
+        if(questions.Count > 0)
+        {
+            SetButtonState(true);
+            SetDefaultButtonSprites();
+            currentQuestion = questions[0];
+            questions.RemoveAt(0);
+            DisplayQuestion();
+        }
+        else
+        {
+            questionText.text = "Quiz Completed!";
+            SetButtonState(false);
+        }
+    }
+
+    void DisplayQuestion()
+    {
+        questionText.text = currentQuestion.getQuestion();
+
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = currentQuestion.getAnswer(i);
+        }
+    }
+
+    void SetButtonState(bool state)
+    {
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    void SetDefaultButtonSprites()
+    {
+        for(int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+        }
+    }
 }
