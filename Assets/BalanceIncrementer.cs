387626ddd0ee5f772a8e6650274881b4d86059b6
Selahaddin Turkoglu <90051755+turkogl3@users.noreply.github.com>
2023-04-11 23:51:47 -0500

    Fixed the balance problem

--- Assets/BalanceIncrementer.cs
+++ Assets/BalanceIncrementer.cs
@@ -6,33 +6,31 @@ public class BalanceIncrementer : MonoBehaviour
 {
     public TextMeshProUGUI Balancetext;
 
-    public int balance = 0;
-
     public event Action<int> BalanceChanged;
 
-    // Start is called before the first frame update
     void Start()
     {
         Balancetext = GetComponent<TextMeshProUGUI>();
-        if (Balancetext != null)
-        {
-            Balancetext.text = "Balance: $" + balance;
-        }
+        UpdateBalanceText();
     }
 
     void Update()
     {
+        UpdateBalanceText();
+    }
+
+    private void UpdateBalanceText()
+    {
         if (Balancetext != null)
         {
-            Balancetext.text = "Balance: $" + balance;
+            Balancetext.text = "Balance: $" + GameManager.Instance.balance;
         }
     }
 
     public void AddBalance(int amount)
     {
-        balance += amount;
-        Update();
-        BalanceChanged?.Invoke(balance); // Invoke the BalanceChanged event, passing in the new balance value
-        Debug.Log("Balance updated. Current balance: $" + balance);
+        GameManager.Instance.AddBalance(amount);
+        UpdateBalanceText();
+        BalanceChanged?.Invoke(GameManager.Instance.balance);
     }
 }
--- Assets/Scripts/Questions/Quiz.cs
+++ Assets/Scripts/Questions/Quiz.cs
@@ -16,18 +16,12 @@ public class Quiz : MonoBehaviour
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
     [SerializeField] Button nextQuestionButton;
-    [SerializeField] BalanceIncrementer balanceIncrementer;
-    public GameObject balanceTracker;
     
-
     int correctAnswerReward = 50;
     int wrongAnswerPenalty = 20;
 
     void Start()
     {
-        
-        balanceIncrementer = balanceTracker.GetComponent<BalanceIncrementer>();
-    
         string quizType = PlayerPrefs.GetString("QuizType", "Computer Science");
         if (quizType == "Math")
         {
@@ -38,11 +32,8 @@ public class Quiz : MonoBehaviour
             questions = CSquestions;
         }
         GetNextQuestion();
-       
     }
 
-
-  
     public void OnAnswerSelected(int index)
     {
         Image buttonImage;
@@ -50,7 +41,7 @@ public class Quiz : MonoBehaviour
         if(index == currentQuestion.getCorrectAnswerIndex())
         {
             questionText.text = "Correct!";
-            balanceIncrementer.AddBalance(correctAnswerReward);
+            GameManager.Instance.AddBalance(correctAnswerReward);
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
         }
@@ -59,7 +50,7 @@ public class Quiz : MonoBehaviour
             int correctAnswerIndex = currentQuestion.getCorrectAnswerIndex();
             string correctAnswer = currentQuestion.getAnswer(correctAnswerIndex);
             questionText.text = "Sorry, the correct answer was;\n" + correctAnswer;
-            balanceIncrementer.AddBalance(-wrongAnswerPenalty);
+            GameManager.Instance.AddBalance(-wrongAnswerPenalty);
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
         }
